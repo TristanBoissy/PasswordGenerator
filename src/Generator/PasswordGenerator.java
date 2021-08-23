b0b70cd463a43e4eff2fcb07bf4956f5bbf6bcf0
TristanBoissy <65125057+TristanBoissy@users.noreply.github.com>
2021-08-23 13:50:31 -0400

    Add files via upload
    Created the new generator algorithm, it randomizes every character of the string and makes sure that it has at least one of all the types of characters which are lowercase and uppercase letters, symbols and numbers.
    The password has a minimum of 10 characters, which can be increased or decreased in the constant variable
    The program also randomize the verification point for each types of characters to make it more secure

--- src/Generator/PasswordGenerator.java
+++ src/Generator/PasswordGenerator.java
@@ -5,41 +5,207 @@ public class PasswordGenerator {
 
     //Contains de newly generated password
     private String newPassword;
+
+
     Random rand = new Random();
 
     /***
      * ASCII table for the letters, numbers and symbols
      */
-    private int LOWERCASE_MIN = 97;
-    private int LOWERCASE_MAX = 122;
+    private final int LOWERCASE_MIN = 97;
+    private final int LOWERCASE_MAX = 122;
 
-    private int UPPERCASE_MIN = 65;
-    private int UPPERCASE_MAX = 90;
+    private final int UPPERCASE_MIN = 65;
+    private final int UPPERCASE_MAX = 90;
 
-    private int SYMBOL_MIN = 33;
-    private int SYMBOL_MAX = 47;
+    private final int SYMBOL_MIN = 33;
+    private final int SYMBOL_MAX = 47;
 
-    private int NUMBER_MIN = 48;
-    private int NUMBER_MAX = 57;
+    private final int NUMBER_MIN = 48;
+    private final int NUMBER_MAX = 57;
+
+    private final int LOWERCASE = 1;
+    private final int UPPERCASE = 2;
+    private final int SYMBOL = 3;
+    private final int NUMBER = 4;
+
+    private final int MIN_PASSWORD_CHARACTERS = 10;
+
+
+    private int lowercaseVerification;
+    private int uppercaseVerification;
+    private int symbolVerification;
+    private int numberVerification;
 
 
 
-
-    public PasswordGenerator(){
-        newPassword = generatePassword();
-    }
 
     public String getPassword(){return this.newPassword;}
 
-    private String generatePassword(){
-        String password = new String();
-        return password;
+    public PasswordGenerator(){
+
+        randomizeCharacterVerification();
+
+        for(int i = 0; i < rand.nextInt(MIN_PASSWORD_CHARACTERS/2)+MIN_PASSWORD_CHARACTERS; i++){
+            if(i == 0){
+                generateFirstCharacter();
+            }
+            generateNewCharacter(verifyCharacters(i));
+        }
+
+    }
+
+    private void generateFirstCharacter(){
+        switch (rand.nextInt(4)+1){
+
+            case LOWERCASE:
+                newPassword = String.valueOf(newLowercaseLetter());
+                break;
+            case UPPERCASE:
+                newPassword = String.valueOf(newUppercaseLetter());
+                break;
+            case SYMBOL:
+                newPassword = String.valueOf(newSymbol());
+                break;
+            case NUMBER:
+                newPassword = String.valueOf(newNumber());
+                break;
+        }
+    }
+
+    private void generateNewCharacter(int verification){
+
+        if(verification == -1){
+            verification = rand.nextInt(4)+1;
+        }
+
+        switch (verification){
+            case LOWERCASE:
+                newPassword += newLowercaseLetter();
+                break;
+            case UPPERCASE:
+                newPassword += newUppercaseLetter();
+                break;
+            case 3:
+                newPassword += newSymbol();
+                break;
+            case 4:
+                newPassword += newNumber();
+                break;
+        }
+    }
+
+    private int verifyCharacters(int i){
+
+        if(i == lowercaseVerification){
+            if(verifyLowercaseLetter()){
+                return -1;
+            }
+            return LOWERCASE;
+        }
+        if(i == uppercaseVerification){
+            if(verifyUppercaseLetter()){
+                return -1;
+            }
+            return UPPERCASE;
+        }
+        if(i == symbolVerification){
+            if(verifySymbol()){
+                return -1;
+            }
+            return SYMBOL;
+        }
+        if(i == numberVerification){
+            if(verifyNumber()){
+                return -1;
+            }
+            return NUMBER;
+        }
+
+        return -1;
+    }
+
+
+    private boolean verifyLowercaseLetter(){
+        return verifyTheString(LOWERCASE_MIN, LOWERCASE_MAX);
+    }
+
+    private boolean verifyUppercaseLetter(){
+        return verifyTheString(UPPERCASE_MIN,UPPERCASE_MAX);
+
+    }
+    private boolean verifySymbol(){
+        return verifyTheString(SYMBOL_MIN,SYMBOL_MAX);
+    }
+    private boolean verifyNumber(){
+        return verifyTheString(NUMBER_MIN,NUMBER_MAX);
+    }
+
+    private boolean verifyTheString(int MIN, int MAX){
+        for(int i = 0; i < newPassword.length(); i++){
+            char character = returnCharacteraAtIndex(i);
+            if(MIN <= character && character <= MAX){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private char returnCharacteraAtIndex(int i){
+        return newPassword.charAt(i);
+    }
+
+
+    private void randomizeCharacterVerification(){
+
+        randomizeLowercaseCharacter();
+        randomizeUppercaseCharacter();
+        randomizeSymbolCharacter();
+        randomizeNumberCharacter();
+    }
+
+    private void randomizeLowercaseCharacter(){
+        lowercaseVerification = rand.nextInt(MIN_PASSWORD_CHARACTERS);
+    }
+
+    private void randomizeUppercaseCharacter(){
+        while(lowercaseVerification != uppercaseVerification){
+
+            uppercaseVerification = rand.nextInt(MIN_PASSWORD_CHARACTERS);
+        }
+    }
+    private void randomizeSymbolCharacter(){
+        while(symbolVerification != uppercaseVerification &&
+                symbolVerification != lowercaseVerification){
+
+            symbolVerification = rand.nextInt(MIN_PASSWORD_CHARACTERS);
+        }
+    }
+
+    private void randomizeNumberCharacter(){
+        while(numberVerification != uppercaseVerification &&
+                numberVerification != lowercaseVerification &&
+                numberVerification != symbolVerification){
+
+            numberVerification = rand.nextInt(MIN_PASSWORD_CHARACTERS);
+        }
     }
 
     private char newLowercaseLetter(){
-
-
-
-
+        return (char) (rand.nextInt(LOWERCASE_MAX-LOWERCASE_MIN)+LOWERCASE_MIN);
     }
+
+    private char newUppercaseLetter(){
+        return (char) (rand.nextInt(UPPERCASE_MAX-UPPERCASE_MIN)+UPPERCASE_MIN);
+    }
+
+    private char newSymbol(){
+        return (char) (rand.nextInt(SYMBOL_MAX-SYMBOL_MIN)+SYMBOL_MIN);
+    }
+
+    private char newNumber(){
+        return (char) (rand.nextInt(NUMBER_MAX-NUMBER_MIN)+NUMBER_MIN);
+    }
+
+
 }
