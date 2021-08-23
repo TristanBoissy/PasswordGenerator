4ab99e2434b837636af8223cf2a843b455390d20
TristanBoissy <65125057+TristanBoissy@users.noreply.github.com>
2021-08-23 13:49:44 -0400

    Add files via upload
    Created the new generator algorithm, it randomizes every character of the string and makes sure that it has at least one of all the types of characters which are lowercase and uppercase letters, symbols and numbers.
    The password has a minimum of 10 characters, which can be increased or decreased in the constant variable
    The program also randomize the verification point for each types of characters to make it more secure

--- src/Generator/PasswordGenerator.java
+++ src/Generator/PasswordGenerator.java
@@ -1,8 +1,29 @@
 package Generator;
+import java.util.Random;
 
 public class PasswordGenerator {
 
-    String newPassword;
+    //Contains de newly generated password
+    private String newPassword;
+    Random rand = new Random();
+
+    /***
+     * ASCII table for the letters, numbers and symbols
+     */
+    private int LOWERCASE_MIN = 97;
+    private int LOWERCASE_MAX = 122;
+
+    private int UPPERCASE_MIN = 65;
+    private int UPPERCASE_MAX = 90;
+
+    private int SYMBOL_MIN = 33;
+    private int SYMBOL_MAX = 47;
+
+    private int NUMBER_MIN = 48;
+    private int NUMBER_MAX = 57;
+
+
+
 
     public PasswordGenerator(){
         newPassword = generatePassword();
@@ -12,7 +33,13 @@ public class PasswordGenerator {
 
     private String generatePassword(){
         String password = new String();
-        password = "allo";
         return password;
+    }
+
+    private char newLowercaseLetter(){
+
+
+
+
     }
 }
